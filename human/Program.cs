84645a11a71b01ff705848182e5d578e5f0888b5
Moses Reuben <46658412+mosesreuben@users.noreply.github.com>
2019-04-05 14:08:32 -0700

    Add files via upload

---
+++ human/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Human
+{
+    public class Human
+    {
+        public string name;
+        public int strength;
+        public int intelligence;
+        public int dexterity;
+        private int health;
+
+        public Human(string name)
+        {
+            this.name = name;
+            strength = 3;
+            dexterity = 3;
+            intelligence = 3;
+            health = 100;
+        }
+
+        public Human(string name, int strength, int intelligence, int dexterity, int health)
+        {
+            this.name = name;
+            this.strength = strength;
+            this.intelligence = intelligence;
+            this.dexterity = dexterity;
+            this.health = health;
+        }
+        public void attack(object target)
+        {
+            Human enemy = target as Human;
+            if(enemy == null)
+            {
+                Console.WriteLine("Failed Attack");
+            }
+            else
+            {
+                enemy.health -= strength * 5;
+            }
+        }
+    }        
+}
